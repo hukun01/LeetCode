b923be85fb4c184a1c92f1b22632f4d663019c10
Kun Hu <kun.hu7602@gmail.com>
2019-03-31 23:15:21 -0700

    2 more graph questions

---
+++ graph/542. 01 Matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        '''
+        Two keys here:
+        1. Update the initial 1s to infinite, so that we know 
+           whether a cell is visited or not. At the same time,
+           store the 0s to a queue for later traversal.
+        2. When updating a neighbor cell, use the current cell's value + 1.
+        '''
+        rows, cols = len(matrix), len(matrix[0])
+        queue = collections.deque()
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] != 0:
+                    matrix[r][c] = float('inf')
+                else:
+                    queue.append((r, c))
+        
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        while queue:
+            r, c = queue.popleft()
+            for d in dirs:
+                newR = r + d[0]
+                newC = c + d[1]
+                if not (0 <= newR < rows and 0 <= newC < cols
+                        and matrix[newR][newC] > matrix[r][c] + 1):
+                    continue
+                queue.append((newR, newC))
+                matrix[newR][newC] = matrix[r][c] + 1
+        
+        return matrix
---
+++ stack/394. Decode String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = [["", 1]]
+        num = ""
+        for c in s:
+            if c.isdigit():
+                num += c
+            elif c == '[':
+                stack.append(["", int(num)])
+                num = ""
+            elif c == ']':
+                subString, repeat = stack.pop()
+                stack[-1][0] += subString * repeat
+            else:
+                stack[-1][0] += c
+        
+        return stack[0][0]
