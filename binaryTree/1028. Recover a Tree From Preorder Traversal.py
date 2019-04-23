cfcd329f4d05168f5cced1ff8a1eded430c4feae
Kun Hu <kun.hu7602@gmail.com>
2019-04-22 21:38:15 -0700

    optimized 1028

--- binaryTree/1028. Recover a Tree From Preorder Traversal.py
+++ binaryTree/1028. Recover a Tree From Preorder Traversal.py
@@ -7,37 +7,28 @@
 
 class Solution:
     def recoverFromPreorder(self, S: str) -> TreeNode:
+        levels = {}
         num = ""
         depth = 0
-        levelNodes = {}
-		# we process the last node and the last depth when we encounter the '-', 
-		# so add a '-' at the end to capture the final node
         S += "-"
         for c in S:
-            if c.isdigit():
+            # continuous '-', keep adding depth
+            if c != '-':
                 num += c
             else:
-				# continuous '-', keep adding depth
                 if not num:
                     depth += 1
                     continue
                 node = TreeNode(int(num))
                 num = ""
-				# root node
-                if not levelNodes:
-                    levelNodes[0] = [node]
-                    depth += 1
-                    continue
-				# after adding root, we always have the parent node as the last node 
-				# from the last level
-                parent = levelNodes[depth - 1][-1]
-                if not parent.left:
-                    parent.left = node
-                else:
-                    parent.right = node
-                if depth not in levelNodes:
-                    levelNodes[depth] = []
-                levelNodes[depth].append(node)
+                if depth not in levels:
+                    levels[depth] = []
+                levels[depth].append(node)
+                if depth > 0:
+                    parent = levels[depth - 1][-1]
+                    if not parent.left:
+                        parent.left = node
+                    else:
+                        parent.right = node
                 depth = 1
-                    
-        return levelNodes[0][0]
+        return levels[0][0]
