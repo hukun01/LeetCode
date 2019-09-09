c396c000f2061a8985cfff7c384d300a429d00bc
Kun Hu <kun.hu7602@gmail.com>
2019-09-08 22:02:27 -0700

    add more new problems

---
+++ depthFirstSearch/301. Remove Invalid Parentheses.py
@@ -0,0 +1,41 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        '''
+        Check the sequence of parenthesis using counter, increment 1 when seeing '(',
+        decrement 1 when seeing ')'. When counter is negative, we have extra ')'.
+        When seeing the first extra ')', recursively process each new string with one ')' removed
+        from each possible position.
+        When deleting ')', remember to skip the continuous duplicates.
+        If there is no extra ')', meaning we have removed them all in the previous part, now we need
+        to scan from right to left to ensure there is no extra '(', or we can exit the function after
+        adding the current string to the result list.
+        '''
+        ans = []
+        def dfs(s, iStart, jStart, openParen, closedParen):
+            counter = collections.Counter()
+            i = iStart
+            while i < len(s) and counter[closedParen] <= counter[openParen]:
+                counter[s[i]] += 1
+                i += 1
+            if counter[closedParen] > counter[openParen]:
+                # put i back to the right position as we incremented it before exiting the loop above,
+                # now i points to the first extra ')'.
+                i -= 1
+                # now there is one extra ')' that we need to delete,
+                # we can go back to delete one ')' from each position, skipping continuous duplicates.
+                for j in range(jStart, i+1):
+                    if s[j] == closedParen and (j == jStart or s[j-1] != closedParen):
+                        # effectively removing s[j], and continue the processing with recursion,
+                        # note that now the first i elements contain valid # of parenthesis.
+                        dfs(s[:j] + s[j+1:], i, j, openParen, closedParen)
+            else:
+                # no extra ')', now we need to scan from right to left, or to exit the whole function.
+                if openParen == '(':
+                    # we've processed from left to right, now reverse
+                    # the input and parenthesis, to process from right to left.
+                    dfs(s[::-1], 0, 0, ')', '(')
+                else:
+                    ans.append(s[::-1])
+        dfs(s, 0,0,'(', ')')
+        return ans
+        
---
+++ general/341. Flatten Nested List Iterator.py
@@ -0,0 +1,104 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.data = nestedList
+        self.pos = 0
+        self.nextIter = None
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if self.nextIter:
+            return self.nextIter.next()
+        else:
+            val = self.data[self.pos].getInteger()
+            self.pos += 1
+            return val
+        
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self.nextIter and self.nextIter.hasNext():
+            return True
+        self.nextIter = None
+        while self.pos < len(self.data):
+            element = self.data[self.pos]
+            if element.isInteger():
+                return True
+            else:
+                self.nextIter = NestedIterator(element.getList())
+                self.pos += 1
+                return self.hasNext()
+        return False
+
+    '''
+    Another style using generators.
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.iter = self.flatten(nestedList)
+        self.nextItem = next(self.iter, None)
+    
+    def flatten(self, items):
+        if isinstance(items, list):
+            for nestedItem in items:
+                for item in self.flatten(nestedItem):
+                    yield item
+        elif items.isInteger():
+            yield items.getInteger()
+        else:
+            for item in self.flatten(items.getList()):
+                yield item
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        currItem = self.nextItem
+        self.nextItem = next(self.iter, None)
+        return currItem
+        
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.nextItem is not None
+    '''
+        
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
