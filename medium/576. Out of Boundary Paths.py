035b598e06fcbf57abb41b2119cad92b4f848d83
Kun Hu <kun.hu7602@gmail.com>
2020-03-10 22:13:26 -0700

    add 576

--- medium/576. Out of Boundary Paths.py
+++ medium/576. Out of Boundary Paths.py
@@ -2,6 +2,27 @@
 class Solution:
     def findPaths(self, m: int, n: int, N: int, i: int, j: int) -> int:
         '''
+        1/2 Memoized DFS
+        '''
+        cache = {}
+        def dfs(r, c, steps):
+            if (r, c, steps) in cache:
+                return cache[(r, c, steps)]
+            if not 0 <= r < m or not 0 <= c < n:
+                return 1
+            if steps == 0:
+                return 0
+            total = 0
+            for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                nr = dr + r
+                nc = dc + c
+                total += dfs(nr, nc, steps - 1)
+            cache[(r, c, steps)] = total
+            return total
+        return dfs(i, j, N) % (10 ** 9 + 7)
+
+        '''
+        2/2 Memoized DFS (another style)
         dp[r][c][n] means the number of paths out of boundary,
         at (r, c) with n moves left.
         '''
