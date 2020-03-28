815d0b9c322bde09c26382c02c65057175f35841
Kun Hu <kun.hu7602@gmail.com>
2020-03-28 11:59:05 -0700

    math

--- hard/1293. Shortest Path in a Grid with Obstacles Elimination.py
+++ hard/1293. Shortest Path in a Grid with Obstacles Elimination.py
@@ -2,33 +2,37 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         '''
-        Use BFS, but we also need to keep track of the remaining eliminationCount
-        as part of the step.
-        Also, since we may come to a position multiple times, we only keep the
-        one with the most eliminationCount. In the visited dict, the last
-        eliminationCount for a position should be the biggest one.
+        Path finding with a bit tweak.
+
+        We may come to a position multiple times, but we are not sure what eliminationCount
+        is each time. This is because there can be a detour in the grid with less
+        elimination needed, comparing to a shorter path but with more elimination.
+        Hence, one key here is to keep track of the (r, c, k) entry instead of just (r, c).
+        Also, we can early stop! If the remaining eliminationCount is greater than the
+        total number of cells, we can just return the shortest path because we can tear down
+        all the obstacles on the path.
+
+        The rest is just like normal path finding. Note that BFS would do the same job.
         '''
-        q = [(0, 0, 0, k)] # steps, r, c, K
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-        for steps, r, c, remainingK in q:
-            #steps, r, c, remainingK = q.popleft()
-            if r == rows - 1 and c == cols - 1:
+        q = [(0, 0, 0, k)] # steps, r, c, k
+        R, C = len(grid), len(grid[0])
+        seen = set()
+        while q:
+            steps, r, c, k = heapq.heappop(q)
+            if grid[r][c] == 1:
+                if k == 0:
+                    continue
+                k -= 1
+            if (r, c) == (R - 1, C - 1):
                 return steps
-            if remainingK >= rows - 1 - r + cols - 1 - c - 1:
-                return steps + rows - 1 - r + cols - 1 - c
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                newR = dr + r
-                newC = dc + c
-                if newR < 0 or newR >= rows or newC < 0 or newC >= cols:
+            if R - 1 - r + C - 1 - c <= k:
+                return steps + R - 1 - r + C - 1 - c
+            for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                nr, nc = dr + r, dc + c
+                if not 0 <= nr < R or not 0 <= nc < C:
                     continue
-                if (remainingK, newR, newC) in visited:
+                if (nr, nc, k) in seen:
                     continue
-                visited.add((remainingK, newR, newC))
-                if grid[newR][newC] == 1:
-                    if remainingK == 0:
-                        continue
-                    q.append((steps + 1, newR, newC, remainingK - 1))
-                else:
-                    q.append((steps + 1, newR, newC, remainingK))
+                seen.add((nr, nc, k))
+                heapq.heappush(q, (steps + 1, nr, nc, k))
         return -1
--- hard/780. Reaching Points.py
+++ hard/780. Reaching Points.py
@@ -5,9 +5,20 @@ class Solution:
         There are many branches from (sx, sy) to (tx, ty), all of which
         would not reach the end, except one path. From backward, there is
         only one path from (tx, ty) to (sx, sy).
-        Assuming tx < ty, we have (tx, ty) -> (tx, ty - tx) ->..-> (sx, sy).
+
+        (tx, ty) comes from either:
+        1. (tx' + k*ty, ty)
+        2. (tx, ty' + k*tx)
+        So tx' = tx % ty, if tx > ty
+        or ty' = ty % tx, if ty > tx
+
         To handle cases like (1, 10000) where tx << ty, we use modulo, 
         otherwise the subtraction would take linear time.
+
+        We keep tranforming tx and ty until tx <= sx or ty <= sy, then
+        we check whether the diffs can be composed by one element, namely,
+        either (sy, ty) % sx == 0 if sx == tx already,
+        or     (sx, tx) % sy == 0 if sy == ty already.
         '''
         while tx > sx and ty > sy:
             if tx < ty:
