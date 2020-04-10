15e8831db251698774eace9c1d3aff7f9421188c
Kun Hu <kun.hu7602@gmail.com>
2020-04-10 14:26:26 -0700

    new DP

---
+++ medium/375. Guess Number Higher or Lower II.py
@@ -0,0 +1,13 @@
+# 375. Guess Number Higher or Lower II
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        @functools.lru_cache(None)
+        def dp(i, j):
+            if i >= j:
+                return 0
+            ans = math.inf
+            for k in range(i, j + 1):
+                ans = min(ans, k + max(dp(i, k-1), dp(k+1, j)))
+            return ans
+            
+        return dp(1, n)
