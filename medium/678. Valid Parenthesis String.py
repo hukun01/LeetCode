586d0a26c3641c8e07810878e9b9c86fe2efb533
Kun Hu <kun.hu7602@gmail.com>
2020-04-16 22:16:00 -0700

    paren problems

---
+++ medium/678. Valid Parenthesis String.py
@@ -0,0 +1,63 @@
+# 678. Valid Parenthesis String
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        '''
+        1/2 Memoized DFS is straightforward and fast enough to pass, but
+        there is another solution with much better time and space complexities.
+        '''
+        @functools.lru_cache(None)
+        def dfs(i, left):
+            for j in range(i, len(s)):
+                if s[j] == '(':
+                    left += 1
+                elif s[j] == ')':
+                    if left == 0:
+                        return False
+                    left -= 1
+                else:
+                    ans = dfs(j + 1, left) or dfs(j + 1, left + 1)
+                    if ans or left > 0 and dfs(j + 1, left - 1):
+                        return True
+            return left == 0
+        return dfs(0, 0)
+        '''
+        2/2 DFS without actually searching.
+        We just want to know *whether* it's possible to be valid, we don't need
+        to know what exact steps to use stars to make the string valid.
+
+        Hence, we keep track of the relative count of left parenthesis, and we record
+        its range - what the lower and upper bound is as we go through the string.
+
+        Use left paren as the base, when we see a right paren, we decrement the count,
+        then it's possible to have a valid string if the lower bound is 0, and upper bound >= 0.
+
+        On the other hand, whenever the upper bound becomes negative, it means no matter how we
+        use stars as as left parens, the right ones outnumber the left, and it's impossible to
+        get a valid string.
+
+        Below code can be refactored to make shorter, but it would be less clear.
+        '''
+        lower = upper = 0
+        for c in s:
+            if c == '(':
+                lower += 1
+                upper += 1
+            elif c == ')':
+                # **This is critical.**
+                # If lower is 0 now, it means we have
+                # used more stars as right parens,
+                # we should take those stars back and use
+                # them as empty, so we can skip this right
+                # paren here.
+                # The upper check will ensure that we don't
+                # have too many right parens.
+                if lower > 0:
+                    lower -= 1
+                upper -= 1
+                if upper < 0:
+                    return False
+            else:
+                if lower > 0:
+                    lower -= 1
+                upper += 1
+        return lower == 0
