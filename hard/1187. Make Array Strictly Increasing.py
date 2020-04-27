ff0622e0b43970be25e845132e6c81d511f23219
Kun Hu <kun.hu7602@gmail.com>
2020-04-26 20:33:19 -0700

    improved 1420, add 1187

---
+++ hard/1187. Make Array Strictly Increasing.py
@@ -0,0 +1,25 @@
+# 1187. Make Array Strictly Increasing
+class Solution:
+    def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
+        '''
+        DP. Similar to the DP solution of 300. Longest Increasing Subsequence.
+        Use a dict to record the mapping of {element: ops}.
+        For each element in arr1, we know the last possible elements and ops.
+        For every possible last element, if the current element is greater than
+        it, we save it and the ops in a current dict; otherwise, we need to find
+        in arr2 the smallest element that's greater than the last element, and ops + 1.
+        '''
+        dp = {-1:0}
+        arr2.sort()
+        for i in arr1:
+            tmp = collections.defaultdict(lambda: float('inf'))
+            for key in dp:
+                if i > key:
+                    tmp[i] = min(tmp[i],dp[key])
+                loc = bisect.bisect_right(arr2,key)
+                if loc < len(arr2):
+                    tmp[arr2[loc]] = min(tmp[arr2[loc]],dp[key]+1)
+            dp = tmp
+        if dp:
+            return min(dp.values())
+        return -1
--- hard/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.py
+++ hard/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.py
@@ -2,66 +2,47 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
         '''
-        Let f[i][j][k] be the answer, number of ways to have a sequence with
-        length being n, current max being j, and has k search cost.
+        1/2 DP.
+        Let f[i][j][c] be the answer, number of ways to have a sequence with
+        length being i, search cost being j, current max being c.
         
         State transition:
-        f[i][j][c] += j * f[i-1][j][c], because if we have (i-1)-length array,
-        we can add anything from [1, j] as the i-th number.
-        f[i][j][c] += SUM of f[i - 1][x][c - 1] where x in [1, j), because
-        if we have arrays with (i-1) length and x from [1, j) as the current max,
-        to build the c-th search cost, we can append element j to all those arrays.
+        f[i][j][c] = item1 + item2
+        item1: c * f[i-1][j][c], because if we have (i-1)-length array,
+               we can add anything from [1, c] as the i-th number.
+        item2: sum(f[i - 1][j - 1][1:c]), because if we have arrays with
+               (i-1) length and x from [1, c) as the current max, to build
+               the j-th search cost, we can append element c to all those arrays.
+
+        Note that f[i] depends on f[i-1], so we can reduce 3D space to 2D.
+        Time complexity is O(nkm^2)
         '''
-        
         MOD = 10 ** 9 + 7
 
-        # 1/3 Regular DP as described above
-        f = [[[0] * (k + 1) for j in range(m + 1)] for i in range(n + 1)]
-        for j in range(1, m + 1):
-            f[1][j][1] = 1
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                for c in range(1, k + 1):
-                    s = j * f[i-1][j][c]
-                    s += sum(f[i - 1][x][c - 1] for x in range(1, j))
-                    f[i][j][c] = (f[i][j][c] + s) % MOD
-        
-        return sum(f[n][j][k] for j in range(1, m + 1)) % MOD
+        f = [[[0] * (m + 1) for j in range(k + 1)] for i in range(n + 1)]
+        for c in range(1, m+1):
+            f[1][1][c] = 1
+        for i in range(2, n+1):
+            for j in range(1, k+1):
+                for c in range(1, m+1):
+                    f[i][j][c] = f[i-1][j][c] * c + sum(f[i-1][j-1][1:c])
+        return sum(f[n][k][1:]) % MOD
 
-        # 2/3 Space optimized DP. Note that f[i] only depends on f[i-1], so
-        # we can reduce space from 3D to 2D. No change to time complexity.
+        '''
+        2/2 Optimized DP, with reduced space (2D) and time (O(nkm)).
+        Space optimization is based on the fact that f[i] depends on f[i-1].
 
-        A = [[0] * (k + 1) for _ in range(m + 1)]
-
-        for j in range(1, m + 1):
-            A[j][1] = 1
-
-        for _ in range(n - 1):
-            B = [[0] * (k + 1) for _ in range(m + 1)]
-
-            for j in range(1, m + 1):
-                for c in range(1, k + 1):
-                    s = j * A[j][c]
-                    s += sum(A[x][c - 1] for x in range(1, j))
-                    B[j][c] = s % MOD
-            A = B
-
-        return sum(A[j][k] for j in range(1, m + 1)) % MOD
-
-        # 3/3 Further optimized time
-        A = [[0] * (k + 1) for _ in range(m + 1)]
-
-        for j in range(1, m + 1):
-            A[j][1] = 1
-
-        for _ in range(n - 1):
-            B = [[0] * (k + 1) for _ in range(m + 1)]
-
-            for c in range(1, k + 1):
-                z = 0
-                for j in range(1, m + 1):
-                    B[j][c] = (z + j * A[j][c]) % MOD
-                    z = (z + A[j][c - 1]) % MOD
-            A = B
-
-        return sum(A[j][k] for j in range(1, m + 1)) % MOD
+        Time optimization is based on the fact that we always get sum([1:c])
+        from the last row in the deepest for loop, so we can just compute it
+        once.
+        '''
+        f1 = [[0] * (m + 1) for j in range(k + 1)]
+        f1[1][1:] = [1] * m
+        for _ in range(2, n+1):
+            f2 = [[0] * (m + 1) for j in range(k + 1)]
+            for j in range(1, k+1):
+                item2s = list(itertools.accumulate(f1[j-1]))
+                for c in range(1, m+1):
+                    f2[j][c] = f1[j][c] * c + item2s[c-1]
+            f1 = f2
+        return sum(f1[k][1:]) % MOD
