c2ce5a84a925d4583c3e1c6f873733bcf0e14eab
Kun Hu <kun.hu7602@gmail.com>
2020-04-29 23:07:01 -0700

    improve

--- medium/238. Product of Array Except Self.py
+++ medium/238. Product of Array Except Self.py
@@ -6,24 +6,20 @@ class Solution:
         Note that the relations between the input and the result are as below:
 
         Numbers:     2    3    4     5
-        Lefts:            2  2*3 2*3*4
-        Rights:  3*4*5  4*5    5
-
-        which is effectively below (filled with '1' on the empty spaces):
-        Numbers:     2    3    4     5
         Lefts:       1    2  2*3 2*3*4
         Rights:  3*4*5  4*5    5     1
 
         We can calculate the lefts in one pass, and multiply that with rights in the second pass.
         Note that we need to store the lefts in the output array to not use extra space.
         '''
+        n = len(nums)
+        ans = [1] * n
         left = 1
-        ans = [1] * len(nums)
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             left *= nums[i-1]
             ans[i] = left
         right = 1
-        for i in reversed(range(len(nums)-1)):
+        for i in range(n-2, -1, -1):
             right *= nums[i+1]
             ans[i] *= right
         return ans
--- medium/494. Target Sum.py
+++ medium/494. Target Sum.py
@@ -2,41 +2,38 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], S: int) -> int:
         '''
         1/2 Subset sum problem.
-                                sum(P) - sum(N) = target
-        sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
-                            2 * sum(P) = target + sum(nums)
-        
+        Let P be the positive numbers, N be the negative numbers. We have below equations:
+                          sum(P) - sum(N) = S
+        sum(P) + sum(N) + sum(P) - sum(N) = S + sum(P) + sum(N)
+                               2 * sum(P) = S + sum(nums)
+                                   sum(P) = (S + total) // 2
+        The problem now is asking us to find the number of positive number subsets
+        whose sum is a target.
+
+        Let f[i][j] be the number of ways to achieve a sum j in the first i numbers.
+        f[i][j] = f[i-1][j] + f[i-1][j-num].
+        Since f[i] only depends on f[i-1], we can reduce the space to 1D.
+        '''
         total = sum(nums)
         if (S + total) % 2 == 1 or total < S:
             return 0
         target = (S + total) // 2
-        
-        # in the first i numbers, to achieve a sum j, we have
-        # ways[i][j] = ways[i-1][j] + ways[i-1][j-num], 
-        # because ways[i][j] uses last row ways[i-1], we can use a 1D array to represent ways.
-        ways = [0 for _ in range(target + 1)]
-        ways[0] = 1
-        
-        for n in nums:
-            for i in reversed(range(n, target+1)):
-                ways[i] += ways[i - n]
-        return ways[-1]
-
+        f = [[0] * (target + 1) for _ in range(len(nums) + 1)]
+        f[0][0] = 1
+        for i in range(1, len(nums) + 1):
+            for j in range(target + 1):
+                f[i][j] = f[i-1][j]
+                if j - nums[i-1] >= 0:
+                    f[i][j] += f[i-1][j - nums[i-1]]
+        return f[-1][-1]
+        '''
         2/2 DFS with memoization
         '''
-        def dfs(start, runningSum, seen):
-            if start == len(nums):
-                if runningSum == S:
+        @functools.lru_cache(None)
+        def dfs(idx, currSum):
+            if idx == len(nums):
+                if currSum == S:
                     return 1
                 return 0
-                
-            # this is used to mark the starting index and runningSum and 
-            # the number of possible ways from here
-            mark = str(start) + "->" + str(runningSum)
-            if mark not in seen:
-                waysIfAdd = dfs(start + 1, runningSum + nums[start], seen)
-                waysIfMinus = dfs(start + 1, runningSum - nums[start], seen)
-                seen[mark] = waysIfAdd + waysIfMinus
-            return seen[mark]
-        
-        return dfs(0, 0, dict())
+            return dfs(idx + 1, currSum + nums[idx]) + dfs(idx + 1, currSum - nums[idx])
+        return dfs(0, 0)
