6fe039c6a44628109591eb20061d70ee97afa9bd
Kun Hu <kun.hu7602@gmail.com>
2020-05-06 07:33:31 -0700

    improve 1340; add 169

---
+++ easy/169. Majority Element.py
@@ -0,0 +1,20 @@
+# 169. Majority Element
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''
+        Brute force approach is trivial. Here is the voting algorithm.
+        We switch candidate to current number whenever its vote is 0.
+        We will eventually switch to the majority number, because it has the most votes.
+        '''
+        vote = 0
+        candidate = None
+
+        for num in nums:
+            if vote == 0:
+                candidate = num
+            if num == candidate:
+                vote += 1
+            else:
+                vote -= 1
+
+        return candidate
--- hard/1340. Jump Game V.py
+++ hard/1340. Jump Game V.py
@@ -2,6 +2,7 @@
 class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         '''
+        1/2 DFS on graph.
         Build the graph with each index pointing to the first higher point from
         left and right. Then use DFS to find the longest path.
         We need to check both sides, instead of only the lower side, because it's
@@ -22,11 +23,31 @@ class Solution:
         check(range(N))
         check(reversed(range(N)))
         
-        cache = {}
+        @functools.lru_cache(None)
         def dfs(i):
-            if i in cache:
-                return cache[i]
-            cache[i] = 1 + max(map(dfs, jumps[i]), default = 0)
-            return cache[i]
+            return 1 + max(map(dfs, jumps[i]), default = 0)
         
         return max(map(dfs, range(N)))
+        '''
+        2/2 DP with monotonic stack.
+        Let f[i] be the answer if we start jumping at index i.
+        All f[i] starts with 1 based on the problem description.
+        f[i] = max(f[i], f[j]) for all j on the left within d distance, and arr[j] < arr[i];
+        f[i] = max(f[i], f[j]) for all j on the right within d distance, and arr[j] < arr[i].
+        '''
+        dp = [1] * (len(arr) + 1)
+        stack = []
+        for i, n in enumerate(arr + [1000000]):
+            while stack and arr[stack[-1]] < n:
+                same_height = [stack.pop()]
+                while stack and arr[stack[-1]] == arr[same_height[0]]:
+                    same_height.append(stack.pop())
+                for j in same_height:
+                    # jump to left
+                    if i - j <= d:
+                        dp[i] = max(dp[i], dp[j] + 1)
+                    # jump to right
+                    if stack and j - stack[-1] <= d:
+                        dp[stack[-1]] = max(dp[stack[-1]], dp[j] + 1)
+            stack.append(i)
+        return max(dp[:-1])
