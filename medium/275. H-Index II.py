0ebd2a81d1fcb16c03deb4006bce011981fa5285
Kun Hu <kun.hu7602@gmail.com>
2020-06-19 12:47:47 -0700

    challenge

---
+++ medium/275. H-Index II.py
@@ -0,0 +1,20 @@
+# 275. H-Index II
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        '''
+        Binary search.
+        '''
+        N = len(citations)
+        l, h = 0, N - 1
+            
+        while l <= h:
+            m = (l + h) // 2
+            hIdx = N - m
+            if citations[m] == hIdx:
+                return hIdx
+            elif citations[m] < hIdx:
+                l = m + 1
+            else:
+                h = m - 1
+                
+        return N - l
