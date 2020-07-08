664ca9a570eda1e771a0130917327a024713c1aa
Kun Hu <kun.hu7602@gmail.com>
2020-07-07 22:46:12 -0700

    add some comments

--- medium/275. H-Index II.py
+++ medium/275. H-Index II.py
@@ -3,10 +3,11 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         '''
         Binary search.
+        To count the number of citations that are greater than the
+        testing value, leverage the *sorted* citations array.
         '''
         N = len(citations)
         l, h = 0, N - 1
-            
         while l <= h:
             m = (l + h) // 2
             hIdx = N - m
@@ -16,5 +17,5 @@ class Solution:
                 l = m + 1
             else:
                 h = m - 1
-                
+
         return N - l
--- medium/279. Perfect Squares.py
+++ medium/279. Perfect Squares.py
@@ -1,6 +1,8 @@
+# 279. Perfect Squares
 class Solution:
     def numSquares(self, n: int) -> int:
-        """ 
+        """
+        1/3 A concise BFS. 
         BFS, also a DP. Note that the size of the input is actually log(n),
         assuming each step takes constant time, then the worst case we need O(N) steps,
         N is the value of the input number. And N's size is log(N), so O(N) = O(2^(log(N))) = O(2^n)
@@ -12,3 +14,40 @@ class Solution:
             sums = {x + y for x in sums for y in squares}
             result += 1
         return result
+        '''
+        2/3 A regular BFS.
+        '''
+        squares = [i * i for i in range(int(n ** 0.5) + 1)]
+        q = deque([0])
+        visited = set()
+        steps = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                for s in squares:
+                    next_node = node + s
+                    if next_node > n:
+                        break
+                    if next_node in visited:
+                        continue
+                    visited.add(next_node)
+                    if next_node == n:
+                        return steps + 1
+                    q.append(next_node)
+            steps += 1
+        '''
+        3/3 Memoized DFS that's equivalent to DP.
+        Note that the squares here is different that it doesn't have 0.
+        '''
+        squares = [i * i for i in range(1, int(n ** 0.5) + 1)]
+        @functools.lru_cache(None)
+        def find(n):
+            if n == 0:
+                return 0
+            ans = n
+            for s in squares:
+                if s > n:
+                    break
+                ans = min(ans, 1 + find(n - s))
+            return ans
+        return find(n)
