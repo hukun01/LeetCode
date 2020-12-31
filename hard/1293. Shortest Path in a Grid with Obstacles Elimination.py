408af7fcded8e34cbb42cfc28bba2b2f3242c485
Kun Hu <kun.hu7602@gmail.com>
2020-12-31 15:25:03 -0800

    update old

--- hard/1293. Shortest Path in a Grid with Obstacles Elimination.py
+++ hard/1293. Shortest Path in a Grid with Obstacles Elimination.py
@@ -4,35 +4,56 @@ class Solution:
         '''
         Path finding with a bit tweak.
 
-        We may come to a position multiple times, but we are not sure what eliminationCount
-        is each time. This is because there can be a detour in the grid with less
-        elimination needed, comparing to a shorter path but with more elimination.
-        Hence, one key here is to keep track of the (r, c, k) entry instead of just (r, c).
-        Also, we can early stop! If the remaining eliminationCount is greater than the
-        total number of cells, we can just return the shortest path because we can tear down
-        all the obstacles on the path.
+        We may come to a position multiple times, but we are not sure what
+        eliminationCount is each time. This is because there can be a detour
+        in the grid with less elimination needed, comparing to a shorter path
+        but with more elimination.
+        Hence, one key here is to keep track of the (r, c, k) entry instead of
+        just (r, c).
+        Note, we can NOT early stop! Even if the remaining eliminationCount is
+        greater than the total number of cells, we can NOT just return the
+        shortest path as 'step + current distance to target', because the
+        current step may not be the shortest one, without trying all possible
+        elimination.
+        However, the idea behind this strategy can still help reduce time
+        massively. We just check at the beginning, if k >= shortest path, then
+        we don't really need to consider any obstacles.
 
-        The rest is just like normal path finding. Note that BFS would do the same job.
+        Note that BFS would do the same job.
+
+        Time: O(V k + E log(V k)) where V is R*C, as we may visit each cell at
+              most k times, each time with a different remaining k.
+        Space: O(V k)
+
+        Note that Dijkstra's algorithm is slower than BFS which takes
+        O(E + V) time. Overall, Dijkstra's is the same as BFS, if the weights
+        among edges are the same. And Dijkstra's can be easily transformed to
+        BFS, by changing the priority queue to a deque.
+        See this post for details on when Dijkstra's is preferred.
+        https://stackoverflow.com/questions/25449781/what-is-difference-between-bfs-and-dijkstras-algorithms-when-looking-for-shorte
         '''
-        q = [(0, 0, 0, k)] # steps, r, c, k
         R, C = len(grid), len(grid[0])
-        seen = set()
-        while q:
-            steps, r, c, k = heappop(q)
-            if grid[r][c] == 1:
-                if k == 0:
-                    continue
-                k -= 1
-            if (r, c) == (R - 1, C - 1):
-                return steps
-            if R - 1 - r + C - 1 - c <= k:
-                return steps + R - 1 - r + C - 1 - c
-            for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+        target = (R - 1, C - 1)
+        if sum(target) <= k:
+            return sum(target)
+
+        pq = [(0, k, 0, 0)] # (step, remaining_k, r, c)
+        visited = set()
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        while pq:
+            step, remaining_k, r, c = heappop(pq)
+            if (r, c) == target:
+                return step
+
+            if (remaining_k, r, c) in visited:
+                continue
+            visited.add((remaining_k, r, c))
+
+            for dr, dc in dirs:
                 nr, nc = dr + r, dc + c
                 if not 0 <= nr < R or not 0 <= nc < C:
                     continue
-                if (nr, nc, k) in seen:
-                    continue
-                seen.add((nr, nc, k))
-                heappush(q, (steps + 1, nr, nc, k))
+                if grid[nr][nc] == 0 or remaining_k - 1 >= 0:
+                    heappush(pq, (step + 1, remaining_k - (grid[nr][nc] == 1), nr, nc))
         return -1
--- hard/1707. Maximum XOR With an Element From Array.py
+++ hard/1707. Maximum XOR With an Element From Array.py
@@ -2,26 +2,21 @@
 class Solution:
     def maximizeXor(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         '''
-        Bit-wise trie + sort.
+        1/2 Bit-wise trie + sort.
         Sort the queries by 'm', for all numbers <= m, add them to the bit-wise
         trie. Then query the trie and greedily find the max possible xor
         between x and any of the existing numbers in trie.
 
         Time: O(n log(n) + q log(q)) where n is len(nums), q is len(queries).
               The trie add() and query() takes O(32) time each time, so
-              overall it adds O(32n + 32q), which is dominated by sort() time.
+              overall it adds O(32n + 32q), which may get dominated by sort()
+              time if n or q is greater 10^32.
         Space: O(n + q)
 
         Similar to
         421. Maximum XOR of Two Numbers in an Array, regarding Trie operations.
         1697. Checking Existence of Edge Length Limited Paths, regarding sort.
         '''
-        queries = sorted(enumerate(queries), key=lambda q: q[1][1])
-        nums.sort()
-        nums_idx = 0
-        n = len(nums)
-        ans = [0] * len(queries)
-
         Trie = lambda: defaultdict(Trie)
         trie = Trie()
 
@@ -45,9 +40,66 @@ class Solution:
                     node = node[i_th_bit]
             return max_xor_num
 
+        queries = sorted(enumerate(queries), key=lambda q: q[1][1])
+        nums.sort()
+        nums_idx = 0
+        n = len(nums)
+        ans = [0] * len(queries)
+
         for q_i, (x, m) in queries:
             while nums_idx < n and nums[nums_idx] <= m:
                 add(nums[nums_idx])
                 nums_idx += 1
             ans[q_i] = query(x)
         return ans
+        '''
+        2/2 Sort + binary search.
+        Sort the nums, and for each query (x, m), find the best possible
+        number within [:bound(m)], which can be located by binary search. The
+        best number has the opposite bit at every possible positions, comparing
+        to x.
+        We start by finding the 'stop' position in nums using 'm'.
+        Similar to 1/2, we try from the most significant bit to the least ones.
+        During the process, we track the currently identified number from its
+        highest bits.
+        At each bit, let 'plus' be current number + bit.
+            1. if nums[start] >= 'plus', meaning all numbers have this bit set,
+               then we have to take it, regardless of x's bit.
+            2. If nums[start] < 'plus', meaning it doesn't have this bit set,
+               we check whether the larger end, nums[stop - 1], has this bit
+               set, if it does, meaning we have a choice to choose a number
+               with this bit set or not, based on what x has on this bit:
+                a. If x has this bit set, we want to find a number in the
+                   smaller half, that has this bit being 0. Hence, discard the
+                   larger half.
+                b. Otherwise, we want to do the opposite.
+            3. If nums[stop - 1] < 'plus', meaning all numbers within range
+               doesn't have this bit set, then we can't use it, regardless of
+               x's bit.
+
+        Time: O(n log(n) + q log(n))
+        Space: O(n + q)
+        Note that this solution is much faster than 1/2, althoug the
+        theorectical complexity is close. This is because space is much much
+        less here, while in 1/2 we need to store up to 2^31 Trie nodes.
+        '''
+        nums.sort()
+
+        def query(x, m):
+            start, stop = 0, bisect_right(nums, m)
+            num = 0
+            for i in range(31, -1, -1):
+                i_th_bit = 1 << i
+                plus = num + i_th_bit
+                if nums[start] >= plus:
+                    num = plus
+                elif nums[stop-1] >= plus:
+                    cut = bisect_left(nums, plus, start, stop)
+                    if x & i_th_bit:
+                        stop = cut
+                    else:
+                        start = cut
+                        num = plus
+            return num ^ x
+
+        return [query(x, m) if nums[0] <= m else -1 for x, m in queries]
