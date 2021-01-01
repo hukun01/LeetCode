775c960613b292a0a722ab8a577b0ec1a981e609
Kun Hu <kun.hu7602@gmail.com>
2021-01-01 10:36:42 -0800

    update

--- easy/270. Closest Binary Search Tree Value.py
+++ easy/270. Closest Binary Search Tree Value.py
@@ -1,29 +1,26 @@
+# 270. Closest Binary Search Tree Value
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def closestValue(self, root, target):
-        """
-        The key here is to always compare the current answer to the current root.val.
-        
-        :type root: TreeNode
-        :type target: float
-        :rtype: int
-        """
-        """ 1/2 Recursive
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        '''
+        The key here is to always compare the current answer to the current
+        root.val.
+        1/2 Recursive
+        '''
         a = root.val
         kid = root.left if target < a else root.right
         if not kid:
             return a
         b = self.closestValue(kid, target)
         return min((b, a), key=lambda x: abs(target - x))
-        """
-        """ 2/2 Iterative
-        """
+        '''
+        2/2 Iterative
+        '''
         closest = root.val
         while root:
             closest = min((root.val, closest), key=lambda x: abs(target - x))
