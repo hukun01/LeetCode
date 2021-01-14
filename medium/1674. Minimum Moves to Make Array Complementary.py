f9bd30eba0540d77bc6d22dea4dbf6895df93e35
Kun Hu <kun.hu7602@gmail.com>
2021-01-14 10:04:50 -0800

    update

--- medium/1674. Minimum Moves to Make Array Complementary.py
+++ medium/1674. Minimum Moves to Make Array Complementary.py
@@ -4,7 +4,7 @@ class Solution:
         '''
         1/3 Difference array.
         Let c be the complementary sum. Different c leads to different
-        number of moves. Let moves[c] be the number of total moves at c.
+        number of moves. Let moves[c] be the number of total moves to reach c.
 
         Let a, b, c be the nums[i], nums[n - 1 - i], a+b, respectively.
         There are 5 cases that requires different numbers of operations
@@ -18,7 +18,7 @@ class Solution:
                change a to limit, b to b+1.
 
         Given the intervals of c that adds 0, 1, 2 ops, our goal is to find
-        the interval with the min accumulated ops. It's similar to applying
+        the point with the min accumulated ops. It's similar to applying
         +0, +1, +2 update to various intervals in the range of [0, 2*limit],
         each interval is updated up to n times. Then find the smallest updated
         point after all updates are done.
@@ -39,10 +39,9 @@ class Solution:
         n = len(nums)
         lower_bound = 2
         diff = [0] * (2 * limit + 2)
-        for i, a in enumerate(nums[:n//2]):
-            b = nums[n - 1 - i]
-            if a > b:
-                a, b = b, a
+        for i in range(n // 2):
+            a, b = sorted([nums[i], nums[n - 1 - i]])
+
             diff[lower_bound] += 2
             # below line is the shortcut to diff[a+1] -= 2 and diff[a+1] += 1.
             diff[a + 1] -= 1
@@ -109,10 +108,23 @@ class Solution:
             3. [a+b+1, b+limit], 1 less number stays the same, -1
             4. [b+limit+1, 2*limit), 1 less number stays the same, -1
         We can do the same difference array technique as in 1/3 to find the max
-        number of elements that stay the same. Alternatively, we can collect
-        those change points as events, and do line sweep to find the same
-        answer. The advantage of the second solution is that it doesn't depend
-        on the value of limit.
+        number of elements that stay the same.
+        
+        n = len(nums)
+        diff = [0] * (2 * limit + 2)
+        for i in range(n // 2):
+            a, b = sorted([nums[i], nums[n - 1 - i]])
+
+            diff[a+1] += 1
+            diff[a+b] += 1
+            diff[a+b+1] -= 1
+            diff[b+limit+1] -= 1
+
+        return n - max(accumulate(diff))
+
+        Alternatively, we can collect those change points as events, and do
+        line sweep to find the same answer. The advantage of the second
+        solution is that it doesn't depend on the value of limit.
 
         Time: O(n log n)
         Space: O(n)
--- medium/332. Reconstruct Itinerary.py
+++ medium/332. Reconstruct Itinerary.py
@@ -1,22 +1,23 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         '''
-        Note that if there is a dead-ended path, there can only be one such path,
-        and the other path would be a circle.
-        Based on this attribute, if we go into a dead end, the previous node must
-        have another branch to go. Also, when we finish the recursion on dead-ended path,
-        we can add the nodes to the route, because they will be the ending part.
+        Note that if there is a dead-ended path, there can only be one such
+        path, and the other path would be a circle.
+        Based on this attribute, if we go into a dead end, the previous node
+        must have another branch to go. Also, when we finish the recursion on
+        dead-ended path, we can add the nodes to the route, because they will
+        be the ending part.
         Remember to reverse the route before returning.
         '''
-        targets = collections.defaultdict(list)
+        targets = defaultdict(list)
         for t in sorted(tickets)[::-1]:
             targets[t[0]].append(t[1])
-        
+
         ans = []
         def visit(airport):
             while targets[airport]:
                 visit(targets[airport].pop())
             ans.append(airport)
-                
+
         visit("JFK")
         return ans[::-1]
