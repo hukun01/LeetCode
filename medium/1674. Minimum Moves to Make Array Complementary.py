43d05c086f6249c2382ea898dbe22dc6c3aecbc2
Kun Hu <kun.hu7602@gmail.com>
2021-01-13 22:47:42 -0800

    updated 1674 a lot

--- medium/1674. Minimum Moves to Make Array Complementary.py
+++ medium/1674. Minimum Moves to Make Array Complementary.py
@@ -2,7 +2,7 @@
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
         '''
-        Difference array.
+        1/3 Difference array.
         Let c be the complementary sum. Different c leads to different
         number of moves. Let moves[c] be the number of total moves at c.
 
@@ -50,3 +50,82 @@ class Solution:
             diff[a + b + 1] += 1
             diff[b + limit + 1] += 1
         return min(accumulate(diff[lower_bound:]))
+        '''
+        2/3 Binary search.
+        Assume in each pair (a, b), a <= b. Similar to logic in 1/3, we have:
+        If target is t, then
+            limit + b < t, we need to increase both numbers, 2 ops
+            a + 1 > t, we need to decrease both numbers, 2 ops
+            a + b == t, we don't do anything, 0 ops
+            the other cases, we need to do one operation, 1 ops
+        We iterate through all possible targets, in [2, 2 * limit + 1].
+        For each target t, we find:
+            the number of pairs that sum to t;
+            the number of pairs that need 2 increase ops;
+            the number of pairs that need 2 decrease ops;
+            the number of pairs that need 1 (inc/dec) ops is n - t.
+        Note that n is the number of pairs = len(nums) // 2, not len(nums).
+        
+        Use binary search to find increase_both and decrease_both. For example,
+        for increase_both, we can find from b's list the number of elements
+        that are < t - limit.
+
+        Then the total ops = n - equal + increase_both + decrease_both,
+        note that (n - equal) includes the number of pairs that need 2 ops, so
+        we just add the 'increase_both' and 'decrease_both' once again.
+
+        Time: O(k log(n) + n log(n))
+        Space: O(n)
+        '''
+        n = len(nums) // 2
+        pairs = [(a, b) for a, b in zip(nums[:n], nums[n:][::-1])]
+        bbs = sorted(max(a, b) for a, b in pairs)
+        aas = sorted(min(a, b) for a, b in pairs)
+        sums = Counter(a + b for a, b in pairs)
+        ans = sys.maxsize
+        for t in range(2, limit * 2 + 1):
+            equal = sums[t]
+            increase_both = bisect_left(bbs, t - limit)
+            decrease_both = n - bisect_left(aas, t)
+            ans = min(ans, n - equal + increase_both + decrease_both)
+        return ans
+        '''
+        3/3 Events and line sweep.
+        Similar to logic in 1/3, but opposite, based on below:
+        There are 5 cases that requires different numbers of operations
+        to update a and/or b to get to c, depending on which interval c
+        falls in. Assume a <= b:
+            1. [2, a], requires 2 ops: change b to 1, a to [1, a-1].
+            2. [a+1, a+b), requires 1 op: change b to [1, b-1].
+            3. [a+b], requires 0 op.
+            4. [a+b+1, b+limit], requires 1 op: change a to [a+1, limit]
+            5. [b+limit+1, 2*limit], requires 2 ops: at least need to
+               change a to limit, b to b+1.
+
+        Instead of looking for min ops, we look for max number of elements that
+        stay the same. Then we have the opposite cases:
+            1. [a+1, a+b), 1 number stays the same, +1
+            2. [a+b], 1 more number stays the same, +1
+            3. [a+b+1, b+limit], 1 less number stays the same, -1
+            4. [b+limit+1, 2*limit), 1 less number stays the same, -1
+        We can do the same difference array technique as in 1/3 to find the max
+        number of elements that stay the same. Alternatively, we can collect
+        those change points as events, and do line sweep to find the same
+        answer. The advantage of the second solution is that it doesn't depend
+        on the value of limit.
+
+        Time: O(n log n)
+        Space: O(n)
+        '''
+        n = len(nums)
+        events = []
+        for i in range(n // 2):
+            a, b = sorted([nums[i], nums[n - 1 - i]])
+
+            events.append((a + 1, 1))
+            events.append((a + b, 1))
+            events.append((a + b + 1, -1))
+            events.append((b + limit + 1, -1))
+
+        events.sort()
+        return n - max(accumulate(delta for _, delta in events))
