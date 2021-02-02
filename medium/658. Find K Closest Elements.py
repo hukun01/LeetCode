27a3b228511f0afd2f16fa0ec8b8c28ae8452907
Kun Hu <kun.hu7602@gmail.com>
2021-02-02 08:32:18 -0800

    update

--- medium/658. Find K Closest Elements.py
+++ medium/658. Find K Closest Elements.py
@@ -12,14 +12,26 @@ class Solution:
         The order of x and arr[m] in the diff calculation should be clear if
         we list out all possible cases:
         1. x, arr[mid], arr[mid + k]
-        2. arr[mid], x, arr[mid + k]
-        3. arr[mid], arr[mid + k], x
+        2. arr[mid], arr[mid + k], x
+        3. arr[mid], x, arr[mid + k]
+
+        The #1 and #2 cases are straightforward. We need to move left in #1,
+        and move right in #2. The tricky part is #3. We use the relative
+        distance between (arr[m], x) and (x, arr[m + k]) to measure which
+        direction we should go.
+        If arr[m] - x + arr[m + k] - x >= 0, means arr[m + k] is relatively
+        large (but may be just good), we should cap h = m; Otherwise, arr[m]
+        is so small that we have to exclude it in the next round, by l = m + 1.
+
+        Now apply #3 logic in #1 and #2, and turns out the #3 logic universally
+        applies to #1 and #2.
         '''
-        l, h = 0, len(arr) - k
+        l = 0
+        h = len(arr) - k
         while l < h:
             m = (l + h) // 2
-            if x - arr[m] <= arr[m + k] - x:
+            if arr[m] - x + arr[m + k] - x >= 0:
                 h = m
             else:
                 l = m + 1
-        return arr[l:l + k]
+        return arr[l: l + k]
