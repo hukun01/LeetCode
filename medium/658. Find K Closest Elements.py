d58b464b8df025087f5598ed17220ba3ba46d46f
Kun Hu <kun.hu7602@gmail.com>
2021-02-01 23:24:06 -0800

    review

--- medium/658. Find K Closest Elements.py
+++ medium/658. Find K Closest Elements.py
@@ -1,20 +1,20 @@
 # 658. Find K Closest Elements
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        """
-        Binary-search for where the resulting elements start in the array. 
+        '''
+        Binary-search for where the resulting elements start in the array.
         It's the first index i so that arr[i] is better than arr[i+k] 
         (with "better" meaning closer to or equally close to x). 
         Then just return the k elements starting there.
 
-        Note that we can't use abs() to get the diffs or it will fail in below case:
+        Note that we can't use abs() for diffs or it will fail in below case:
         [1,1,2,2,2,3,3], k = 2, x = 3
-        The order of x and arr[m] in the diff calculation should be clear if we list out
-        all possible cases:
+        The order of x and arr[m] in the diff calculation should be clear if
+        we list out all possible cases:
         1. x, arr[mid], arr[mid + k]
         2. arr[mid], x, arr[mid + k]
         3. arr[mid], arr[mid + k], x
-        """
+        '''
         l, h = 0, len(arr) - k
         while l < h:
             m = (l + h) // 2
