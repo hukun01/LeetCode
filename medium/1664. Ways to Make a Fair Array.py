203ca6c8ff8aa51e112fc2170622768776263c8c
Kun Hu <kun.hu7602@gmail.com>
2021-03-05 16:58:04 -0800

    update

--- medium/1664. Ways to Make a Fair Array.py
+++ medium/1664. Ways to Make a Fair Array.py
@@ -1,54 +1,20 @@
 # 1664. Ways to Make a Fair Array
 class Solution:
-    def waysToMakeFair(self, nums: List[int]) -> int:
+    def waysToMakeFair(self, A: List[int]) -> int:
         '''
-        1/2 Prefix sums.
-        Calculate the prefix sums for even-index and odd-index nums.
-        At each index i:
-            new even sum = even_sums[:i] + odd_sums[i+1:]
-            new odd sum  = odd_sums[:i] + even_sums[i+1:]
-        Add one if new even sum == new odd sum.
+        Sliding split.
+        Let s1 record the even and odd sums on the left;
+        Let s2 record the even and odd sums on the right.
 
-        Time: O(N) where N is the length of the nums array.
-        Space: O(N).
+        Time: O(N) where N is len(A).
+        Space: O(1).
         '''
-        n = len(nums)
-        odds = [0] * (n + 1)
-        evens = [0] * (n + 1)
-        for i, a in enumerate(nums):
-            if i % 2 == 0:
-                evens[i + 1] = evens[i] + a
-                odds[i + 1] = odds[i]
-            else:
-                evens[i+1] = evens[i]
-                odds[i + 1] = odds[i] + a
+        s1, s2 = [0, 0], [sum(A[0::2]), sum(A[1::2])]
         ans = 0
-        for i in range(n):
-            new_even_sum = evens[i] + odds[-1] - odds[i+1]
-            new_odd_sum = odds[i] + evens[-1] - evens[i+1]
-            ans += new_even_sum == new_odd_sum
-        return ans
-        '''
-        2/2 Prefix diff sums.
-        Calculate the (even - odd) prefix sums.
-        At each index i: new diffs = diffs[:i] - diffs[i+1:]
-        [(even - odd) before i] + [(odd(new even) - even(new odd)) after i]
-        Add one if new diffs becomes 0.
+        for i, a in enumerate(A):
+            s2[i % 2] -= a
+            if s1[0] + s2[1] == s1[1] + s2[0]:
+                ans += 1
+            s1[i % 2] += a
 
-        Time and space complexities are the same as 1/2, but this approach is
-        cleaner as there's less confusion on index.
-        '''
-        n = len(nums)
-        diffs = [0] * (n + 1)
-        for i in range(n):
-            if i % 2 == 0:
-                diffs[i+1] = diffs[i] + nums[i]
-            else:
-                diffs[i+1] = diffs[i] - nums[i]
-        ans = 0
-        for i in range(n):
-            diff_before_i = diffs[i]
-            diff_after_i = -(diffs[n] - diffs[i + 1])
-            ans += (diff_before_i + diff_after_i == 0)
         return ans
-    
