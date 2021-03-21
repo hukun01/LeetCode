cd67008c0d57995ad172154b0448928f2b09e28a
Kun Hu <kun.hu7602@gmail.com>
2021-03-21 13:05:44 -0700

    update

---
+++ medium/1798. Maximum Number of Consecutive Values You Can Make.py
@@ -0,0 +1,26 @@
+# 1798. Maximum Number of Consecutive Values You Can Make
+class Solution:
+    def getMaximumConsecutive(self, coins: List[int]) -> int:
+        '''
+        Sort + accumulate.
+        A number can only be made by the smaller numbers, so we sort coins.
+        Let 'cur' be the number we are trying to make, and we already made
+        a consecutive internval [0, cur-1]. Iterate through the coins from
+        small to large values, for each coin c, if we add c to cur, that means
+        we can make a consecutive interval [c, cur+c-1], and if c <= cur, then
+        we can make a consecutive interval [0, cur+c-1]. Otherwise, if c > cur,
+        the [0, cur+c-1] can't be consecutive, and 'cur' is the first number we
+        can't make.
+
+        Time: O(n log(n)) where n is len(coins)
+        Space: O(n)
+        '''
+        coins.sort()
+        cur = 1
+        for c in coins:
+            if c <= cur:
+                cur += c
+            else:
+                break
+
+        return cur
