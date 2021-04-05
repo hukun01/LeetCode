f568b7194a048ca6c0002fe0747e7085ffa0c120
Kun Hu <kun.hu7602@gmail.com>
2021-04-04 20:16:53 -0700

    update

--- arsenal.py
+++ arsenal.py
@@ -154,3 +154,75 @@ class UnionFind:
         self.size[x0] += self.size[y0]
         self.component_count -= 1
         return True
+
+
+class SegTreeNode:
+    '''
+    Segment Tree with lazy propagation.
+    In general, a segment tree handles range operations:
+    1. Range queries: range minimum/maximum query (RMQ), range sum query (RSQ)
+    2. Range update (with lazy propagation)
+
+    Also, single point update and single point query is supported via range of
+    size 1.
+
+    This particular template handles range sum query. For RMQ it can be updated
+    accordingly.
+
+    Also see: https://leetcode.com/articles/a-recursive-approach-to-segment-trees-range-sum-queries-lazy-propagation/
+    '''
+    def __init__(self, a, b):
+        # This node covers interval [a, b] inclusively.
+        self.a = a
+        self.b = b
+        self.left = self.right = None
+        self.val = 0
+        self.lazy = 0
+
+    def __repr__(self):
+        # This is for debug only
+        return f'[{self.a, self.b}] with val {self.val} and lazy {self.lazy}\nleft: {self.left}\nright: {self.right}'
+
+    def init(self, a, b):
+        if a == b:
+            return
+        m = (a + b) // 2
+        self.left = SegTreeNode(a, m)
+        self.right = SegTreeNode(m + 1, b)
+        self.left.init(a, m)
+        self.right.init(m + 1, b)
+
+    def update_range(self, a, b, val):
+        # Update is applied to the first node that completely covered by
+        # [a, b], this way we don't go to each leaf node, and achieve
+        # O(log(n)) time range update.
+        if self.a >= a and self.b <= b:
+            self.lazy += val * (self.b - self.a + 1)
+            return
+
+        if a > self.b or b < self.a or self.a == self.b:
+            return
+
+        self.left.update_range(a, b, val)
+        self.right.update_range(a, b, val)
+    
+    def query_range(self, a):
+        # When querying, we need to push down lazy value from upper nodes to
+        # bottom nodes. Note that we need to split the lazy values from the
+        # parent node to its children nodes based on each child's size.
+        if self.left is None:
+            self.val += self.lazy
+            self.lazy = 0
+            if a == self.a: # if true, it also means self.a == self.b
+                return self.val
+            return 0
+
+        if b < self.a or a > self.b:
+            return 0
+
+        if self.lazy != 0:
+            self.left.lazy += self.lazy * (self.left.b - self.left.a + 1) // (self.b - self.a + 1)
+            self.right.lazy += self.lazy * (self.right.b - self.right.a + 1) // (self.b - self.a + 1)
+            self.lazy = 0
+
+        return self.left.query_range(a, b) + self.right.query_range(a, b)
--- medium/370. Range Addition.py
+++ medium/370. Range Addition.py
@@ -29,3 +29,70 @@ class Solution:
             diff[s] += inc
             diff[e + 1] -= inc
         return list(accumulate(diff[:-1]))
+        '''
+        2/2 Segment Tree. See arsenal.py file for more Segment Tree details.
+        '''
+        root = SegTreeNode(0, length-1)
+        root.init(0, length-1)
+        for s, e, inc in updates:
+            root.update_range(s, e, inc)
+
+        return [root.query_range(i, i) for i in range(length)]
+
+class SegTreeNode:
+    def __init__(self, a, b):
+        # This node covers interval [a, b] inclusively.
+        self.a = a
+        self.b = b
+        self.left = self.right = None
+        self.val = 0
+        self.lazy = 0
+
+    def __repr__(self):
+        # This is for debug only
+        return f'[{self.a, self.b}] with val {self.val} and lazy {self.lazy}\nleft: {self.left}\nright: {self.right}'
+
+    def init(self, a, b):
+        if a == b:
+            return
+        m = (a + b) // 2
+        self.left = SegTreeNode(a, m)
+        self.right = SegTreeNode(m + 1, b)
+        self.left.init(a, m)
+        self.right.init(m + 1, b)
+
+    def update_range(self, a, b, val):
+        # Update is applied to the first node that completely covered by
+        # [a, b], this way we don't go to each leaf node, and achieve
+        # O(log(n)) time range update.
+        if self.a >= a and self.b <= b:
+            self.lazy += val * (self.b - self.a + 1)
+            return
+
+        if a > self.b or b < self.a or self.a == self.b:
+            return
+
+        self.left.update_range(a, b, val)
+        self.right.update_range(a, b, val)
+    
+    def query_range(self, a, b):
+        # When querying, we need to push down lazy value from upper nodes to
+        # bottom nodes. Note that we need to split the lazy values from the
+        # parent node to its children nodes based on each child's size.
+        if self.left is None:
+            self.val += self.lazy
+            self.lazy = 0
+            if a == self.a: # if true, it also means self.a == self.b
+                return self.val
+            return 0
+
+        if b < self.a or a > self.b:
+            return 0
+
+        if self.lazy != 0:
+            total_range_size = self.b - self.a + 1
+            self.left.lazy += self.lazy * (self.left.b - self.left.a + 1) // total_range_size
+            self.right.lazy += self.lazy * (self.right.b - self.right.a + 1) // total_range_size
+            self.lazy = 0
+
+        return self.left.query_range(a, b) + self.right.query_range(a, b)
