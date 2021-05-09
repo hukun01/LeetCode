ede138fbc007e811cc3a808125d25b5b25d8b372
Kun Hu <kun.hu7602@gmail.com>
2021-05-08 21:31:41 -0700

    weekly contest

---
+++ medium/1856. Maximum Subarray Min-Product.py
@@ -0,0 +1,19 @@
+# 1856. Maximum Subarray Min-Product
+class Solution:
+    def maxSumMinProduct(self, nums: List[int]) -> int:
+        '''
+        Mono stack.
+
+        Same as 84. Largest Rectangle in Histogram
+        '''
+        prefix_sums = list(accumulate([0] + nums + [0]))
+        left = []
+        maxArea = 0
+        for i, h in enumerate(nums + [0]):
+            start = i
+            while left and h <= left[-1][0]:
+                prevH, prevStart = left.pop()
+                maxArea = max(maxArea, (prefix_sums[i] - prefix_sums[prevStart]) * prevH)
+                start = prevStart
+            left.append((h, start))
+        return maxArea % (10 ** 9 + 7)
