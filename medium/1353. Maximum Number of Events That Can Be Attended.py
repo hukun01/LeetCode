a8b44f85abb1513c5ee2f43889f3be721b9db80b
Kun Hu <kun.hu7602@gmail.com>
2021-06-15 08:41:45 -0700

    update

--- medium/1353. Maximum Number of Events That Can Be Attended.py
+++ medium/1353. Maximum Number of Events That Can Be Attended.py
@@ -8,26 +8,30 @@ class Solution:
         ending one.
         Note that the earliest ending event may not be compatible if it ends
         earlier than d, so we need to skip it.
+        Also, if there's not available events at the beginning, we should set
+        d = events[i][0] otherwise there would be no events being added in the
+        next step.
 
         Time: O(n log(n)) where n is len(events)
         Space: O(n)
         '''
         events.sort()
+        available_events = []
         ans = 0
         day = 1
-        available_events = []
-
         i = 0
         while i < len(events) or available_events:
+            if not available_events:
+                day = events[i][0]
+
             while i < len(events) and events[i][0] <= day:
                 heappush(available_events, events[i][1])
                 i += 1
 
-            while available_events:
-                current_event_end = heappop(available_events)
-                if current_event_end >= day:
-                    ans += 1
-                    break
+            heappop(available_events)
             day += 1
+            ans += 1
+            while available_events and available_events[0] < day:
+                heappop(available_events)
 
         return ans
