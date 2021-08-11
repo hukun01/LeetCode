37083438df5de38d0b0bfa7161b710f917408830
Kun Hu <kun.hu7602@gmail.com>
2021-08-10 23:12:54 -0700

    adding ruby version solution

--- medium/621. Task Scheduler.py
+++ medium/621. Task Scheduler.py
@@ -10,16 +10,16 @@ class Solution:
         In the first case, the answer is just the length of tasks.
 
         In the second case, each period's size is (n + 1), and there are
-        (f_max - 1) such periods, where f_max is the max frequency of the
+        (max_f - 1) such periods, where max_f is the max frequency of the
         tasks. Note that there can be multiple tasks with the same
         max frequency, imagine they get combined into one task (X)
-        with length being n_max, where n_max is the number of the
-        most frequent tasks. In addition to filling the period, n_max
-        also gets lined up at the end, so the total length would be
-        (f_max - 1) * (n + 1) + n_max
+        with length being max_f_key_num, where max_f_key_num is the number of
+        the most frequent tasks. In addition to filling the period,
+        max_f_key_num also gets lined up at the end, so the total length would
+        be (max_f - 1) * (n + 1) + max_f_key_num
         '''
-        freq = list(Counter(tasks).values())
-        f_max = max(freq)
-        n_max = sum(c == f_max for c in freq)
+        freqs = list(Counter(tasks).values())
+        max_f = max(freqs)
+        max_f_key_num = sum(c == max_f for c in freqs)
 
-        return max(len(tasks), (f_max - 1) * (n + 1) + n_max)
+        return max(len(tasks), (max_f - 1) * (n + 1) + max_f_key_num)
---
+++ medium/621. Task Scheduler.rb
@@ -0,0 +1,12 @@
+# 621. Task Scheduler
+# @param {Character[]} tasks
+# @param {Integer} n
+# @return {Integer}
+def least_interval(tasks, n)
+    freqs = Hash.new(0)
+    tasks.each {|a| freqs[a] += 1}
+    max_f = freqs.values.max
+    max_f_key_num = (freqs.select {|k,v| v == max_f }).length
+
+    return [tasks.length, (max_f - 1) * (n + 1) + max_f_key_num].max
+end
