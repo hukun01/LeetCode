e55fa3088b3a80f5cf7e4db373a28bd221fcd966
Kun <kun.hu7602@gmail.com>
2026-09-16 19:17:13 -0700

    Update 71. Simplify Path.py

--- medium/71. Simplify Path.py
+++ medium/71. Simplify Path.py
@@ -1,6 +1,15 @@
 # 71. Simplify Path
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        '''
+        The key is to be detail oriented, carefully evaluate each
+        situation before committing.
+        Note that when the current dir is at the root, going to the
+        parent dir, aka, '..', is still valid.
+
+        Since we skip '', we won't have the root dir in the final stack,
+        so we should always add it to the head of the path.
+        '''
         ans = []
         for p in path.split('/'):
             if p in { '', '.' }:
