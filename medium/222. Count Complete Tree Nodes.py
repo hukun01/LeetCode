6f424ea0b113e77fbb1e96098127593eeccee56f
Kun <kun.hu7602@gmail.com>
2026-09-21 18:02:06 -0700

    Update 222. Count Complete Tree Nodes.py

--- medium/222. Count Complete Tree Nodes.py
+++ medium/222. Count Complete Tree Nodes.py
@@ -12,26 +12,32 @@ class Solution:
         Based on the property of complete tree, if the right subtree's
         height (determined by its left chain) is one less than the total
         height h, left subtree is full at the h-th level, and we can add
-        the count of left subtree, and continue the process on the right
-        subtree.
-        Otherwise, the right subtree is full at the (h-1)-th level, we can
-        add the count of right subtree, and continue the process on the
-        left subtree.
-        Note that we let the root level to have 0 height.
+        the full count of left subtree nodes, and continue the process on
+        the right subtree.
+        Otherwise, the right subtree is full, we can add the count of right
+        subtree (2**right_subtree_h), and continue the process on the left subtree.
+        
+        Each subtree includes the current root.
         '''
         def get_height(node):
             if not node:
-                return -1
+                return 0
             return 1 + get_height(node.left)
 
         nodes = 0
         h = get_height(root)
         while root:
-            if get_height(root.right) == h - 1:
-                nodes += 1 << h
+            # left subtree's height is one less than the total height
+            # this is by definition of a complete tree
+            left_subtree_h = h - 1
+            right_subtree_h = get_height(root.right)
+            if right_subtree_h == left_subtree_h:
+                # left subtree is full, count all the nodes including current root
+                nodes += 1 << left_subtree_h
                 root = root.right
             else:
-                nodes += 1 << (h - 1)
+                # right subtree is full, count all the nodes including current root
+                nodes += 1 << right_subtree_h
                 root = root.left
             h -= 1
         return nodes
