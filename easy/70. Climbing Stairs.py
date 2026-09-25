acf0c350cb1d9686b33ed732e8115985136a9eec
Kun <kun.hu7602@gmail.com>
2026-09-25 13:42:13 -0700

    Update 70. Climbing Stairs.py

--- easy/70. Climbing Stairs.py
+++ easy/70. Climbing Stairs.py
@@ -3,10 +3,19 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         '''
         Same as the 746. Min Cost Climbing Stairs, just without cost.
+        'a' denotes the #ways to reach the (i-2)th stair;
+        'b' denotes the #ways to reach the (i-1)th stair.
+
+        Reaching the (i)th stair can be done by
+        1. taking 1 step from (i-1)th stair;
+        2. taking 2 steps from (i-2)th stair.
+        So let 'c' denote the #ways to reach the (i)th stair, c = a + b.
+        Then we roll forward, 'a' moves from (i-2)th to (i-1)th, aka, a = b.
+        Similarly b = c.
         '''
-        # 'a' denotes the #ways to start from the i-2th stair;
-        # 'b' denotes the #ways to start from the i-1th stair.
         a = b = 1
-        for _ in range(2, n+1):
-            a, b = b, a + b
+        for _ in range(2, n + 1):
+            c = a + b
+            a = b
+            b = c
         return b
